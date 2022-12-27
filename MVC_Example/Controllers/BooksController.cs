e4448e26f032762dc766c49526458564e880267f
Chad Moore <charles.moore.iv@gmail.com>
2022-12-27 13:27:31 -0500

    Initial Commit of MVC in ASP.NET

---
+++ MVC_Example/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public BooksController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        #region APICalls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.ID == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+        #endregion
+    }
+}
