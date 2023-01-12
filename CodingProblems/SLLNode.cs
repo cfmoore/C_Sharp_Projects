3f406294b1c4d5d250f5d0ebc3a5c5f88f77bc31
Chad Moore <charles.moore.iv@gmail.com>
2023-01-12 15:40:27 -0500

    Add files via upload

---
+++ CodingProblems/SLLNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingProblems
+{
+    public class SLLNode
+    {
+        public int val;
+        public SLLNode next;
+
+        public SLLNode(int val = 0, SLLNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+
+
+        public SLLNode RemoveDuplicates(SLLNode head)
+        {
+            if (head == null)//If nothing is passed
+                return head;
+            if (head.next == null)//
+                return head;
+            SLLNode cur = head.next;
+            SLLNode prev = head;
+            bool found = false;
+            while (cur != null)
+            {
+                Console.WriteLine("prev.val: " + prev.val + "    temp.val:" + cur.val);
+                found = false;
+                if (prev.val == cur.val)
+                {
+                    found = true;
+                    prev.next = cur.next;
+
+                }
+                if (!found)
+                {
+                    prev = prev.next;
+                    cur = cur.next;
+                }
+                else
+                    cur = cur.next;
+            }
+            return head;
+        }
+
+        public void Printer(SLLNode prev)
+        {
+            while (prev != null)
+            {
+                Console.Write(prev.val + " ");
+                prev = prev.next;
+            }
+            Console.WriteLine();
+            return;
+        }
+
+    }
+}
---
+++ CodingProblems/Stairs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingProblems
+{
+    class Stairs
+    {
+        //1 <= num <= 45
+        public int ClimbStairs(int num)
+        {
+            /*
+             * Climb satirs where you can go either 1 or 2 at a time
+             * How many combinations are there for each amount of sqaures.
+             * 
+             * Sequence will follow fibonachi sequence
+             * 0  1  2  3  4  5  6    7   8
+             * 1  1  2  3  5  8  13  21  34  ......
+             */
+            int ways = 1;
+            int old = 1;
+            int temp = 0;
+            //0 & 1 will not run through loop, it will kick it out.
+            //fibonachi sequence
+            for (int i = 1; i < num; i++)
+            { 
+                temp = old;
+                old = ways;
+                ways = old + temp;
+            }
+            return ways;
+        }
+    }
+}
